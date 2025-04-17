9cd3d6a8dc3c02206d9e77f8bbd537890903b578
unknown <ejrubio5@gmail.com>
2025-04-17 10:42:00 -0500

    Added ProxyGenereator, Maya Utils, and AddPathToMaya files

--- src/MayaUtils.py
+++ src/MayaUtils.py
@@ -2,6 +2,51 @@ from PySide2.QtWidgets import QMainWindow, QWidget
 from PySide2.QtCore import Qt
 import maya.OpenMayaUI as omui
 import shiboken2
+import maya.cmds as mc
+
+def IsMesh(obj):
+    shapes = mc.listRelatives(obj, s=True)
+    if not shapes:
+        return False
+    
+    for s in shapes:
+        if mc.objectType(s) == "mesh":
+            return True
+
+    return False
+
+def IsSkin(obj):
+    return mc.objectType(obj) == "skinCluster"
+
+def IsJoint(obj):
+    return mc.objectType(obj) == "joint"
+
+def GetUpperStream(obj):
+    return mc.listConnections(obj, s=True, d=False, sh=True)
+
+def GetLowerStream(obj):
+    return mc.listConnections(obj, s=False, d=True, sh=True)
+
+def GetAllConnectionsIn(obj, nextFunc, filter = None):
+    allFound = set()
+    nexts = nextFunc(obj)
+    searchDepth = 100
+    while nexts and searchDepth > 0:
+        searchDepth -= 1
+        for next in nexts:
+            allFound.add(next)
+
+        nexts = nextFunc(nexts)
+        if nexts:
+            nexts = [x for x in nexts if x not in allFound]
+    
+    if not filter:
+        return list(allFound)
+    
+    filtered = []
+    for found in allFound:
+        if filter(found):
+            filtered.append(found)
 
 def GetMayaMainWindow()->QMainWindow:
     mainWindow = omui.MQtUtil.mainWindow()
